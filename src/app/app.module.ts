dec58e6e273d061f6547402ec687e1d807754eea
Pablo <delatorre.pablodaniel@gmail.com>
2022-12-17 01:27:36 -0300

    avances en formulario, preparando conección con api back

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TecnologiasComponent } from './components/tecnologias/tecnologias.compo
 import { FormularioComponent } from './components/formulario/formulario.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { TecnologiasBackComponent } from './components/tecnologias-back/tecnologias-back.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,9 @@ import { TecnologiasBackComponent } from './components/tecnologias-back/tecnolog
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
+  form:FormGroup;
+  constructor(private formBuilder:FormBuilder) {
 
-  constructor() { }
+    this.form=this.formBuilder.group(
+      {
+        nombre:['',Validators.required],
+        asunto:['',Validators.required],
+        email:['',[Validators.required,Validators.email]],
+        telefono:[,Validators.required]
+      }
+    )
+
+  }
 
   ngOnInit(): void {
+  }
+
+  get Email(){
+    return this.form.get('email')
+  }
+  
+  get Nombre(){
+    return this.form.get('nombre')
+  }
+
+  get Asunto(){
+    return this.form.get('asunto')
+  }
+  
+  get Telefono(){
+    return this.form.get('telefono')
   }
 
 }
